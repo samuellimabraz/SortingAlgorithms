f713136bd8a5fd6d14110db494d6e91254b50499
samuellimabraz <samuellimabraz@gmail.com>
2023-10-05 11:56:00 -0300

    refact: merge sort

--- include/MergeSort.h
+++ include/MergeSort.h
@@ -19,20 +19,17 @@ public:
 
             int i = 0, j = 0, k = 0;
             while (i < mid && j < n - mid) {
-                if (left[i] < right[j]) {
+                if (left[i] < right[j]) 
                     data[k++] = left[i++];
-                } else {
+                else 
                     data[k++] = right[j++];
-                }
             }
 
-            while (i < mid) {
+            while (i < mid) 
                 data[k++] = left[i++];
-            }
-
-            while (j < n - mid) {
-                data[k++] = right[j++];
-            }
+            
+            while (j < n - mid) 
+                data[k++] = right[j++];   
         }
     }
 
